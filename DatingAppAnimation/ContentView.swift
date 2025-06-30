24f8e2853073635e5c088af1ec74119a32dd0118
Meenal Mahajan <meenalmahajan1912@gmail.com>
2025-06-30 15:14:59 +0530

    Initial Commit

---
+++ DatingAppAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DatingAppAnimation
+//
+//  Created by Meenal Mahajan on 30/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DatingAppAnimation/DatingAppAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingAppAnimationApp.swift
+//  DatingAppAnimation
+//
+//  Created by Meenal Mahajan on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct DatingAppAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
